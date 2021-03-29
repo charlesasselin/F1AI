f947c31a4283e3a03c149947631d0275a6935aba
Charles Asselin <charles@asselin.xyz>
2021-03-29 15:43:37 -0400

    Main.py Socket setup for UDP in St-Ubalde, Portneuf, Québec

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
-from racingdata import RacingData
+"""
+Listen to telemetry packets and print them to standard output
+"""
+import socket
 
-practiceData =
+from f1_2020_telemetry.packets import unpack_udp_packet
 
-inst = RacingData()
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_socket.bind(("0.0.0.0", 20777))
+
+while True:
+    udp_packet = udp_socket.recv(2048)
+    packet = unpack_udp_packet(udp_packet)
+    data =  packet._fields_
+    # for name, info in data:
+    #     if name == 'totalLaps':
+    # print("Received:", packet._fields_[1])
