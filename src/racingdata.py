d3f020876bef8205a8d15108f3a98a9419d0d355
Charles Asselin <charles@asselin.xyz>
2021-04-06 16:57:47 -0400

    Working Version 3.1 for static purposes and static data (Decision making main.py and record.py)
        -Lap estimation class has been fully automatized with incoming data currently hardcoded.
        -Data is now plotted as scatter plot with trendlines. Equations for these trendlines are then used in the 3rd AMPL model. This new feature is HUGE because it lets us predict the wear of tyres and their effects on laptimes.
    
    #TODO:
        -Analyze saved data directly from .txt file or sql file
        -Remake 2D AMPL solver as new class and .mod
        -Make unittests
    
    Signed-off-by: Charles Asselin <charles@asselin.xyz>

--- src/racingdata.py
+++ src/racingdata.py
@@ -1,4 +1,6 @@
 from src.data import Data
+import numpy as np
+import matplotlib.pyplot as plt
 import f1_2020_telemetry.packets as f1
 
 
@@ -30,9 +32,34 @@ class RacingData(Data):
     def compound2021():
         return ["Soft", "Medium", "Hard"]
 
+    def plotdata(self):
+        g1 = (self.tyreUsageData[0], self.lapData[0])
+        g2 = (self.tyreUsageData[1], self.lapData[1])
+        g3 = (self.tyreUsageData[2], self.lapData[2])
+        data = (g1, g2, g3)
+        colors = ("red", "blue", "black")
+        groups = ("Soft", "Medium", "Hard")
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        estimateinfo = {'coefficients': [], 'average': []}
+        for data, color, group in zip(data, colors, groups):
+            x, y = data
+            ax.scatter(x, y, alpha=0.8, c=color, edgecolors='none', s=30, label=group)
+            z = np.polyfit(x, y, 1)
+            p = np.poly1d(z)
+            ax.plot(x, p(x), c=color, label="y=%.6fx+(%.6f)" % (z[0], z[1]))
+            estimateinfo['coefficients'].append(z[1])
+            estimateinfo['average'].append(z[0])
+        plt.title('Tyre wear evolution in Austria for Soft, Medium and Hard compounds')
+        plt.legend(loc="best", labelspacing=0.5, borderpad=0.2, handletextpad=0.05)
+        plt.xlabel("Tyre Usage")
+        plt.ylabel("Lap Times")
+        #plt.show()
+        return estimateinfo
+
     def estimatedata(self):
-        coefficients = [1.1265, 1.797, 0.9527]
-        average = [69.711, 67.863, 70.023]
+        coefficients = self.plotdata()['coefficients']
+        average = self.plotdata()['average']
         estimate = {tyre : [coeff, avg]
                     for tyre in self.compound2021()
                     for coeff in coefficients
